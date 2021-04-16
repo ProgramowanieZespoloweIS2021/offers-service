4139a91f50b5a66460d38fa8338fa7430c69e2ca
arokasprz100 <arkadiuszkasprzak97@gmail.com>
2021-04-17 00:01:18 +0200

    Remove the SpringBootTest annotated class (as there is no db for testing now)

--- src/test/java/com/pz/offersservice/OffersServiceApplicationTests.java
+++ src/test/java/com/pz/offersservice/OffersServiceApplicationTests.java
@@ -3,11 +3,11 @@ package com.pz.offersservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class OffersServiceApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//@SpringBootTest
+//class OffersServiceApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
